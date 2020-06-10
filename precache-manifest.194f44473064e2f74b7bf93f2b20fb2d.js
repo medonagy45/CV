94a93ffd2c79dde29f67df07d621033d84b5349d
medonagy45 <medonagy45@gmail.com>
2020-06-10 21:55:50 +0200

    Updates

---
+++ precache-manifest.194f44473064e2f74b7bf93f2b20fb2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a089c7f3f9f50a58b77e6fa92325c25",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "218698c40c1eec8ab7f8",
+    "url": "/CV/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e6348a7c69528638ec1e",
+    "url": "/CV/static/css/main.034db54c.chunk.css"
+  },
+  {
+    "revision": "218698c40c1eec8ab7f8",
+    "url": "/CV/static/js/2.c467cf3f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CV/static/js/2.c467cf3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6348a7c69528638ec1e",
+    "url": "/CV/static/js/main.98f3eb61.chunk.js"
+  },
+  {
+    "revision": "03d8d8e878aaea4997b0",
+    "url": "/CV/static/js/runtime-main.b825d64b.js"
+  },
+  {
+    "revision": "007cf46bdb0c3bccce4118a06bde3d72",
+    "url": "/CV/static/media/Lato-Italic.007cf46b.ttf"
+  }
+]);
